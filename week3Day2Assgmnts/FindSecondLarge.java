c2770bab93913a743f3ed7c18056542fe37f786d
Karpagalaxmi <139650605+Karpagalaxmi@users.noreply.github.com>
2023-08-02 22:50:45 +0530

    Add files via upload

---
+++ week3Day2Assgmnts/FindSecondLarge.java
@@ -0,0 +1,21 @@
+package week3Day2Assgmnts;
+
+import java.util.Arrays;
+
+public class FindSecondLarge {
+		/*
+			 Pseudo Code:
+			 1) Arrange the array in ascending order
+			 2) Pick the 2nd element from the last and print it
+			 */
+			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Here is the input
+		int[] data = {3,2,11,4,6,7};
+		Arrays.sort(data);
+		int len = data.length;
+		System.out.println(data[len-2]);
+	}
+
+}
